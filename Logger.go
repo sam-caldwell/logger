7489ed61a29ccc10fec59469b8168c95e21bc55d
Sam Caldwell <github@samcaldwell.net>
2024-08-01 21:10:22 -0500

    document Logger

--- Logger.go
+++ Logger.go
@@ -6,9 +6,14 @@ import (
 
 // Logger - A simple Logger facility
 type Logger struct {
-	target    Target
+	// target - The device to which log messages will be written
+	target Target
+	// verbosity - quiet or normal gate for quickly turning logging on/off.
 	verbosity Verbosity
-	level     Levels
-	format    string
-	lock      sync.Mutex
+	// level - log level definition consistent with RFC5424
+	level Levels
+	// format - the format string used to ensure uniformity among log lines
+	format string
+	// lock - a lock used in some cases to ensure asynchronous logging activity doesn't overlap
+	lock sync.Mutex
 }
