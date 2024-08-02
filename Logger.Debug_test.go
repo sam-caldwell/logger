be7b725b5a80e33d8644e2cd1a8d1313d58acfaa
Sam Caldwell <github@samcaldwell.net>
2024-08-02 10:14:29 -0500

    add test for Logger.Debug()

---
+++ Logger.Debug_test.go
@@ -0,0 +1,66 @@
+package logger
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestLogger_Debug(t *testing.T) {
+	var log Logger
+
+	verifyData := func(out string, expectEmpty bool) error {
+		if expectEmpty {
+
+		} else {
+
+			parts := strings.Split(out, " ")
+			rhs := strings.Join(parts[2:], " ")
+			const expected = "{\"p\":\"   debug\",m:\"test\"}\n"
+			if rhs != expected {
+				return fmt.Errorf("expected result not found\n"+
+					"  Actual: '%s'\n"+
+					"Expected: '%s'", rhs, expected)
+			}
+		}
+		return nil
+	}
+
+	t.Run("Debug in Debug Level (expect log)", func(t *testing.T) {
+		var buffer bytes.Buffer
+		log.UseBuffer(&buffer)
+		if err := log.SetLevel(Debug); err != nil {
+			t.Fatal(err)
+		}
+		log.Debug("test")
+		if err := verifyData(buffer.String(), false); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("Debug in Info level (expect log)", func(t *testing.T) {
+		var buffer bytes.Buffer
+		log.UseBuffer(&buffer)
+		if err := log.SetLevel(Info); err != nil {
+			t.Fatal(err)
+		}
+		log.Debug("test")
+		if err := verifyData(buffer.String(), true); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("Debug in critical level (no log expected)", func(t *testing.T) {
+		var buffer bytes.Buffer
+		log.UseBuffer(&buffer)
+		if err := log.SetLevel(Critical); err != nil {
+			t.Fatal(err)
+		}
+		log.Debug("test")
+		if err := verifyData(buffer.String(), true); err != nil {
+			t.Fatal(err)
+		}
+
+	})
+}
