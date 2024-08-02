109a34155c372817ab5ba2258d9d8f2a8eb5cc6e
Sam Caldwell <github@samcaldwell.net>
2024-08-01 22:34:47 -0500

    update documentation

--- Logger.SetTarget.go
+++ Logger.SetTarget.go
@@ -1,6 +1,9 @@
 package logger
 
-// SetTarget - Set the current target of a logger object
+// SetTarget - Set the current target of a logger object.
+//
+//	If multiple targets are set to true, only the right most true value
+//	will be configured for logging.
 func (log *Logger) SetTarget(stdout, stderr, syslog bool) (err error) {
 	if stdout {
 		log.UseStdout()
